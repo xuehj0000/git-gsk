9fe21ca36dd125c721b5a7612d2c8b2494578b14
xuehj0000 <15040519070@163.com>
2021-12-10 15:07:40 +0800

    创建项目

---
+++ GSK.Auth.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GSK.Auth.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
