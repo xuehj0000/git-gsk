2bc017da509fe84cdaf5dde18468d968118010e1
xuehj0000 <15040519070@163.com>
2021-12-10 16:09:45 +0800

    11

--- GSK.Auth.Web/Controllers/HomeController.cs
+++ GSK.Auth.Web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using GSK.Auth.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GSK.Auth.Web.Controllers
 {
